c8b7472efb4bc5d58baa8d556d2947856d19ef33
Andres <andresbasur98@gmail.com>
2020-08-29 17:59:27 +0200

    docs agregado

---
+++ docs/precache-manifest.674a1fe9947e22267c0dbbd71566f3ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89c2c35299333867284fcb701d50357",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f046a0e9c0af419a3a9b",
+    "url": "/static/css/main.c5138b73.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f046a0e9c0af419a3a9b",
+    "url": "/static/js/main.c500522b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
